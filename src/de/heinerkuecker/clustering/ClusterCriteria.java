db4165b005335fea3e15b17ce8530c88697c78cc
Heiner Kücker <User@DESKTOP-S47CIRC.fritz.box>
2021-01-25 11:52:20 +0100

    comments

--- src/de/heinerkuecker/clustering/ClusterCriteria.java
+++ src/de/heinerkuecker/clustering/ClusterCriteria.java
@@ -1,7 +1,22 @@
 package de.heinerkuecker.clustering;
 
+/**
+ * Cluster criteria.
+ * Interface to implement.
+ *
+ * @param <E>
+ * 		Type of elements to be checked whether they belong to the same cluster.
+ * 		Typ der Elemente, die geprüft werden sollen, ob sie zum selben Cluster gehören.
+ *
+ * @author Heiner K&uuml;cker
+ */
 public interface ClusterCriteria<E>
 {
+    /**
+     * @param e0 element to check
+     * @param e1 element to check
+     * @return given elements belong to the same cluster or not
+     */
     boolean isInSameCluster(
             final E e0 ,
             final E e1 );
